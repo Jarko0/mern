21e5787c8d4ffdfa7103d001c4c00c354a07211d
Jarko0 <kuba.jarosz123987@gmail.com>
2025-05-28 20:54:23 +0200

    mern

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const userRoutes = require("./routes/users")
+const authRoutes = require("./routes/auth")
+
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+
+const connection = require('./db')
+connection()
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Nasłuchiwanie na porcie ${port}`))
